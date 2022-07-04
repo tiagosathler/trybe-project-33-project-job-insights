461dfbebef74de9a6549274244c085734242adfe
Tiago <sathler@gmail.com>
2022-07-04 15:00:51 -0300

    :test_tube: [test_counter.py]: implements the test of the 'read_brazilian_file' function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,17 @@
-# from src.brazilian_jobs import read_brazilian_file
+from inspect import trace
+from src.brazilian_jobs import read_brazilian_file
+from unittest.mock import mock_open, patch
+from pytest import fixture
 
 
-def test_brazilian_jobs():
-    pass
+@fixture
+def mock_file_data():
+    return "titulo,salario,tipo\n" "Maquinista,2000,trainee"
+
+
+def test_brazilian_jobs(mock_file_data):
+    with patch("builtins.open", mock_open(read_data=mock_file_data)):
+        expected = [
+            {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+        ]
+        assert read_brazilian_file("dummy")[0] == expected[0]
