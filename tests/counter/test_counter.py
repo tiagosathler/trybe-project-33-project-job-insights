3cd62449cd2cef7ef3e2cce74dd0708152d8c237
Tiago <sathler@gmail.com>
2022-07-04 14:32:17 -0300

    :test_tube: [test_counter.py]: implements the test of the 'count_ocurrences' function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+from pytest import fixture
 
 
-def test_counter():
-    pass
+@fixture
+def mock_file_data():
+    return (
+        "python javascript react Python JavaScript PYTHON JAVASCRIPT"
+    )
+
+
+def test_counter(mock_file_data):
+    with patch("builtins.open", mock_open(read_data=mock_file_data)):
+        assert count_ocurrences("dummy", "python") == 3
